e75a565360f7f1c193f21204e2ed564e4afbba77
JoePreviti <35278054+JoePreviti@users.noreply.github.com>
2018-03-01 13:27:03 -0600

    Add files via upload

---
+++ GuiDemo.m
@@ -0,0 +1,15 @@
+%WE WANT SNAP TO GRID (correctly)
+%panning
+%drag and drop
+%dumb it down a little
+%check this out in GUI
+%GUIDE: snap; drag/drop; axes
+
+f=figure;
+
+
+drag_drop(f);
+
+
+
+%drag and drop
