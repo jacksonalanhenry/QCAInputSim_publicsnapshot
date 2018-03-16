ab8cb32b5d3cb331b1cbe66912413c68ccfa94d2
JamesBousquet <35613685+JamesBousquet@users.noreply.github.com>
2018-03-16 16:48:31 -0500

    Added subsref and subasgn and CellID

--- QCACircuit.m
+++ QCACircuit.m
@@ -25,6 +25,7 @@ classdef QCACircuit
         function obj = addNode( obj, newcell )
             n_old = length(obj.Device);
             obj.Device{n_old+1} = newcell;
+            obj.Device{n_old+1}.CellID = length(obj.Device);
             
         end
         
@@ -33,34 +34,79 @@ classdef QCACircuit
         function NeighborCheck( obj ) %Checks for each cell
             
             obj.RefinedDevice = obj.Device;
-            if nargin > 0
-                for n = 1:length(obj.Device)
-                    normarray = [];
-                    Stationary = obj.Device{n};
-                    
-                    for m = 1:length(obj.Device)
-                        if m ~= n
-                            Comparison = obj.Device{m};
-                            normarray(m) = norm(Stationary.CenterPosition - Comparison.CenterPosition);
-                            
-                            
-                        end
-                    end
-                    SmallestNorm = find(normarray == min(normarray)); %returns indicie of smallest normalized vector
-                    obj.RefinedDevice{n} = obj.Device{SmallestNorm};
-                    
-                end
+            for n = 1:length(obj.Device)
+                normarray = [];
+                Stationary = obj.Device{n};
                 
+                for m = 1:length(obj.Device)
+                    if m ~= n
+                        Comparison = obj.Device{m};
+                        normarray(m) = norm(Stationary.CenterPosition - Comparison.CenterPosition);
+                        
+                        
+                    end
+                end
+                SmallestNorm = find(normarray == min(normarray)); %returns indicie of smallest normalized vector
+                obj.RefinedDevice{n+1} = obj.Device{SmallestNorm};
+                
+            end
+            
+        end
+        
+        
+        function obj = CircuitDraw(obj, targetAxes)
+            hold on
+            for CellIndex = 1:length(obj.Device)
+                obj.Devcice{CellIndex} = obj.Device{CellIndex}.ThreeDotDraw(targetAxes);
+                
+            end
+            
+            hold off
+        end
+        
+        function sref = subsref(obj,s)
+            % obj(index) is the same as obj.Device(index)
+            
+            switch s(1).type
+                case '.'
+                    sref = builtin('subsref',obj,s);
+                case '()'
+                    
+                case '{}'
+                    if length(s) < 2
+                        sref = builtin('subsref',obj.Device,s);
+                        return
+                    else
+                        sref = builtin('subsref',obj,s);
+                    end
             end
         end
         
         
         
+        function obj = subasgn(obj,s,val)
+            if isempty(s) && isa(val,'QCACircuit')
+                obj = QCACircuit(val.Device,val.Description);
+            end
+            switch s(1).type
+                case '.'
+                    obj = builtin('subsasgn',obj,s,val);
+                case '()'
+                    
+                case '{}'
+                    if length(s)<2
+                        if isa(val,'QCACircuit')
+                            error('Error: Invalid Indexing')
+                        elseif isa(val,'double')
+                            % Redefine the struct s to make the call: obj.Device(i)
+                            snew = substruct('.','Device','()',s(1).subs(:));
+                            obj = subsasgn(obj,snew,val);
+                        end
+                    end
+            end
+        end
         
-        
-        
-        
-        
+
         
         
     end
