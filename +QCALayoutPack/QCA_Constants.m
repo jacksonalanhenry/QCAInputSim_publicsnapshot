03d6f6c5ec8ce588105b0d85cb7c20ef31efb806
jacksonalanhenry <jhenry11094@gmail.com>
2018-03-28 11:55:04 -0500

    Adding a Constants Package

---
+++ +QCALayoutPack/QCA_Constants.m
@@ -0,0 +1,11 @@
+classdef QCA_Constants
+    properties (Constant)
+        
+        
+        epsilon_0 = 8.854E-12; % Vacuum Permitivity [C/(V*m)]
+        qeV2J = 1.602E-19;     % Conversion factor or Charge of Electron[J]
+        qe = 1;                % Charge of Electron[eV]
+        
+
+    end
+end
