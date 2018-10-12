8539096ea7230135543a7a7ee38d68779c8cf507
Jackson Henry <jhenry11094@gmail.com>
2018-10-12 17:49:53 -0500

    Added UpdatePolarization Function
    beginning the process of periodic drivers

--- PipelineVisualization.m
+++ PipelineVisualization.m
@@ -69,8 +69,8 @@ nx = 125;
 xq = linspace(xmin-1, xmax+1, nx);
 yq = linspace(ymin-2, ymax+2, nt);
 
-if (length(signalList) == 1)
-    clockSignal = signalList{1};
+if (length(clockSignalList) == 1)
+    clockSignal = clockSignalList{1};
 else
     error('Too many signals')
 end
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ This program will be a layout designer for QCA Cells.
 This program will simulate input to Drivers using Electric Fields
 This program will simulate be clocking of circuits using Electric Fields 
 
+
+Text added
+
